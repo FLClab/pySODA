4962fc3ec009523ff4b4758787a756cf6b645cd5
reber199 <38052704+reber199@users.noreply.github.com>
2023-07-20 11:04:53 -0400

    Update run_SODA.py

--- run_SODA.py
+++ run_SODA.py
@@ -7,8 +7,8 @@ OUTPUT_DIRECTORY = r"./example_output"  # Path in which to save outputs
 
 # For ROI mask generation
 ROI_THRESHOLD = 2.0  # Multiplier of ROI threshold. Higher value = more pixels taken.
-CHANNEL_MASK = None # Channel to use as mask. This channel won't be used for SODA analysis. Set to None to generate mask from all channels.
-REMOVE_CHANNEL = None # Channel to remove from SODA analysis (for example, channel used for mask generation). Set to None to remove no channel.
+CHANNEL_MASK = None # Channel to use as mask. This channel won't be used for SODA analysis. Set to None to generate the mask from the sum of all channels.
+REMOVE_CHANNEL = None # Channel to remove from SODA analysis (for example, channel used for mask generation). Set to None to keep all channels.
 
 # For spot detection
 # Channel 2 is not used for a 2 color image
@@ -29,7 +29,6 @@ MIN_AXIS_LENGTH = [3,  # Channel 0  # Minimum length of both ellipse axes of spo
 N_RINGS = 16  # Number of rings around spots (int)
 RING_WIDTH = 1  # Width of rings in pixels
 SELF_SODA = False  # Set to True to compute SODA for couples of spots in the same channel as well
-CONTOUR = False  # Set to False to avoid considering the cell contour
 
 # Display and graphs
 SAVE_ROI = True  # Set to True to save TIF images of spots detection and masks in OUTPUT_DIRECTORY
